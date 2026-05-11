125dcc1ff9b7ce20d18ad09c8d0e42229e8ceeb8
Realeboha <alfrednthaks@gmail.com>
2026-05-11 11:54:41 +0200

    fixed home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -117,7 +117,7 @@ export default function Home({ onNavigate }) {
         <div className="hero-lines" />
 
         {/* Discord button - top right */}
-        <div style={{ position: 'absolute', top: '1.25rem', right: '1.5rem', zIndex: 2 }}>
+        <div style={{ position: 'absolute', top: '5rem', right: '1.5rem', zIndex: 2 }}>
           <a
             href="https://discord.gg/aeTpRuPzcb"
             target="_blank"
